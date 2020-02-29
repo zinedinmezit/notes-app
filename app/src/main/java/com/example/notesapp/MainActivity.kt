1059017bf7c90e5b4d87ab4d83311eb1deedb025
Lizarazu <zinedin.mezit.98@gmail.com>
2020-02-29 19:05:11 +0100

    Add - CalendarFragment,CreateNoteFragment,Bottom navigation navigates between fragments. Action bar removed

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -2,8 +2,14 @@ package com.example.notesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.notesapp.fragments.CalendarFragment
+import com.example.notesapp.fragments.CreateNoteFragment
+import com.example.notesapp.fragments.MainFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +21,57 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         findViewById<BottomNavigationView>(R.id.bottom_nav)
             .setupWithNavController(navController)
+
+        val bottomNav : BottomNavigationView = findViewById(R.id.bottom_nav)
+        bottomNav.setOnNavigationItemSelectedListener {
+            lateinit var fragment : Fragment
+            when(it.itemId){
+                R.id.createNote ->{
+                    fragment = CreateNoteFragment()
+                    loadFragment(fragment)
+                    true
+                }
+
+                R.id.calendar ->{
+                    fragment = CalendarFragment()
+                    loadFragment(fragment)
+                    true
+                }
+
+                R.id.appHome ->{
+                    fragment = MainFragment()
+                    loadFragment(fragment)
+                    true
+                }
+                else -> false
+            }
+
+        }
+
+    }
+
+    public fun loadFragment(_fragment : Fragment)
+    {
+
+        val currentFragment = findCurrentFragment()
+        val _fragmentManager : FragmentManager = supportFragmentManager
+
+        _fragmentManager.beginTransaction()
+            .replace(currentFragment.id,_fragment)
+            .commit()
+
+    }
+
+    public fun findCurrentFragment():Fragment{
+
+        lateinit var currentFragment : Fragment
+        val _fragmentManager : FragmentManager = supportFragmentManager
+        val _fragments : List<Fragment> = _fragmentManager.fragments
+        for(x in _fragments)
+        {
+            if(x.isVisible)
+                currentFragment=x
+        }
+        return currentFragment
     }
 }
---
+++ app/src/main/java/com/example/notesapp/fragments/CreateNoteFragment.kt
@@ -0,0 +1,23 @@
+package com.example.notesapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.notesapp.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class CreateNoteFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+         return inflater.inflate(R.layout.fragment_create,container,false)
+    }
+
+}

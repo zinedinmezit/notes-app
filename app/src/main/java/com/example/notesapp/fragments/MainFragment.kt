3a2b3b125d0395e251e0a6006a49285f1ee5fe7c
Lizarazu <zinedin.mezit.98@gmail.com>
2020-03-27 23:46:15 +0100

    NoteDetails layout - UI changes #2, ActionBar removed, animation for RV item added

--- app/src/main/java/com/example/notesapp/fragments/MainFragment.kt
+++ app/src/main/java/com/example/notesapp/fragments/MainFragment.kt
@@ -5,16 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.DefaultItemAnimator
 import com.example.notesapp.R
 import com.example.notesapp.adapters.NoteAdapter
 import com.example.notesapp.adapters.NoteListener

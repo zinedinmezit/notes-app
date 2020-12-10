694fba33b2bfcf109328daf6e217fab4c93ece47
zinedinmezit <zinedin.mezit@edu.fit.ba>
2020-12-10 14:28:30 +0100

    Calendar - required files init

---
+++ app/src/main/java/com/example/notesapp/adapters/CalendarNotesAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.notesapp.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class CalendarNotesAdapter : RecyclerView.Adapter<CalendarNotesViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CalendarNotesViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: CalendarNotesViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
+
+class CalendarNotesViewHolder(view : View) : RecyclerView.ViewHolder(view){
+
+}
